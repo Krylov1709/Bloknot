785c111a5549c3e658ebcbfe3da79cd33a0abf14
Крылов Алексей <krylov1709@yandex.ru>
2023-06-04 18:17:47 +0300

    Переопределение стандартной модели пользователя на собственную и добавление её в админку

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from apps.accounts.models import User
 
-# Register your models here.
+
+@admin.register(User)
+class MyUserAdmin(UserAdmin):
+    model = User
+    fieldsets = [
+        ("Пользователь", {"fields": ("username", "email")}),
+        ("Личные данные", {"fields": ('sex', "date_of_birth", 'is_active')})
+    ]
+    list_display = ['id', 'username', 'email', 'sex', "date_of_birth", 'is_active']
+
--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,43 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+SEX = (
+    ('MAN', 'Мужской'),
+    ('WOMAN', 'Женский'),
+)
+
+
+class User(AbstractUser):
+    """
+    Создаем новую модель пользователя на основе стандартной
+    и дополняем её своими полями
+    """
+    email = models.EmailField(
+        max_length=30,
+        unique=True
+    )
+    sex = models.CharField(
+        verbose_name='Пол',
+        choices=SEX,
+        max_length=7
+    )
+    date_of_birth = models.DateField(
+        verbose_name='День рождения',
+        null=True,
+        blank=True
+    )
+    is_active = models.BooleanField(
+        verbose_name='Активен',
+        default=True,
+    )
+
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = "Список пользователей"
+        ordering = ('email',)
